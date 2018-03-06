611cee1c6269f24e070e42220e52de027b844824
Nikhil <nikhil.celtics@gmail.com>
2018-03-07 01:31:49 +0530

    added url model

---
+++ models/url.js
@@ -0,0 +1,14 @@
+const instance = require('instance.js');
+
+function Url = {};
+
+Url.update = (id, urlObj) => {
+  const query = { _id : id };
+  return new Promise((resolve, reject) => {
+    instance.collection('list').updateOne(query, myObj, (err, result) => {
+      if (err)
+        throw err;
+      resolve(result);
+    });
+  });
+}
