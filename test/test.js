dba6e6c865e69e6a5a2c4f98444192d1f7ea4418
Nikhil <nikhil.celtics@gmail.com>
2018-03-13 20:17:01 +0530

    adds more tests

--- test/test.js
+++ test/test.js
@@ -1,45 +1,86 @@
 var expect = require('chai').expect;
 var request = require('request');
 var config = require('../config/config.json');
-let urlObj = {
-  url: config.url + ":" + (config.port || 4321),
-};
+const rootUrl = config.url + ":" + (config.port || 4321);
 
 describe('Url Monitor API', () => {
 
-  describe('Ping root url', () => {
+  describe('Get /', () => {
+    let urlObj = {
+      url: rootUrl,
+      method: "get"
+    }
 
     it('returns status 200', (done) => {
-      urlObj.method = "get";
       request(urlObj, (err, response, body) => {
         expect(response.statusCode).to.equal(200);
-        delete urlObj.method;
         done();
       });
     });
 
-  });
-
-  describe('Post a url', () => {
-
-    it('returns post response', (done) => {
-      urlObj.method = "post";
-      urlObj.form = {
-        url: "https://www.google.com",
-        data: "{'a', 'b'}",
-        method: "get"
-      }
+    it('returns array of url objects', (done) => {
       request(urlObj, (err, response, body) => {
         body = JSON.parse(body);
-        expect(body.url).to.equal(urlObj.form.url);
-        expect(body.data).to.equal(urlObj.form.data);
-        expect(body.method).to.equal(urlObj.form.method);
-        delete urlObj.method;
-        delete urlObj.form;
+        // array of urls
+        expect(body).to.be.a('array');
         done();
       })
     })
+  });
 
+  describe('get url data', () => {
+    let urlObj = {
+      url: rootUrl + "/",
+      method: "get"
+    }
+console.log(urlObj);
+    describe('request invalid url', () => {
+      urlObj.url += 'dummy_invalid_id';
+      // even though invalid url, request is handled
+      it('returns status 200', (done) => {
+        request(urlObj, (err, response, body) => {
+          expect(response.statusCode).to.equal(200);
+          done();
+        })
+      })
+
+      it('returns success false', (done) => {
+        request(urlObj, (err, response, body) => {
+          body = JSON.parse(body);
+          expect(body.success).to.equal(false);
+          done();
+        })
+      })
+    })
   })
 
+  describe('Post a url', () => {
+    let urlObj = {
+      url: rootUrl + "/",
+      method: "post",
+      form: {
+        url: "https://www.google.com",
+        data: "{'a', 'b'}",
+        method: "get",
+        headers: "{'content-type': 'application/x-www-form-urlencoded'}"
+      }
+    }
+
+    it('returns post response', (done) => {
+      request(urlObj, (err, response, body) => {
+        body = JSON.parse(body);
+        // checking keys
+        expect(body).to.have.property('url');
+        expect(body).to.have.property('data');
+        expect(body).to.have.property('method');
+        expect(body).to.have.property('headers');
+        // checking values
+        expect(body.url).to.equal(urlObj.form.url);
+        expect(body.data).to.equal(urlObj.form.data);
+        expect(body.method).to.equal(urlObj.form.method);
+        expect(body.headers).to.equal(urlObj.form.headers);
+        done();
+      })
+    })
+  })
 });
