30c2f13e17486cda916b9a179e63371fbfa7e50c
Nikhil <nikhil.celtics@gmail.com>
2018-03-13 18:30:15 +0530

    test bug fix

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ describe('Url Monitor API', () => {
 
   describe('Ping root url', () => {
 
-    it('returns status 200', () => {
+    it('returns status 200', (done) => {
       urlObj.method = "get";
       request(urlObj, (err, response, body) => {
         expect(response.statusCode).to.equal(200);
@@ -22,7 +22,7 @@ describe('Url Monitor API', () => {
 
   describe('Post a url', () => {
 
-    it('returns post response', () => {
+    it('returns post response', (done) => {
       urlObj.method = "post";
       urlObj.form = {
         url: "https://www.google.com",
@@ -30,9 +30,10 @@ describe('Url Monitor API', () => {
         method: "get"
       }
       request(urlObj, (err, response, body) => {
-        expect(body.url).to.equal(urlObj.body.url);
-        expect(body.data).to.equal(urlObj.body.data);
-        expect(body.method).to.equal(urlObj.body.method);
+        body = JSON.parse(body);
+        expect(body.url).to.equal(urlObj.form.url);
+        expect(body.data).to.equal(urlObj.form.data);
+        expect(body.method).to.equal(urlObj.form.method);
         delete urlObj.method;
         delete urlObj.form;
         done();
